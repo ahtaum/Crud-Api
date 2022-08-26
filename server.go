8a0e23b9668b02248e7aeb5f6a9b5ea3fbc37f02
ahtaum <aditalmahdi@gmail.com>
2022-08-26 14:59:16 +0700

    add CORS

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// MyMind "random_words/randoms"
 	route "random_words/routes"
 
 	"github.com/labstack/echo/v4"
@@ -11,8 +10,17 @@ import (
 func main() {
 	e := echo.New()
 
+	// LOGGING
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	// CORS
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:8080", "http://localhost:8080"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+
+	// ROUTES
 	route.AllRoutes(e)
 
 	e.Logger.Fatal(e.Start(":8080"))
